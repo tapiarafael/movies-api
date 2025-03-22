562a0958390fcb978b588d32fec7e67c0e9fe3bd
Rafael Tapia <rafaelsilvatapia@gmail.com>
2025-03-22 19:11:36 -0300

    chore: add env for test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SeederModule } from './seeder/seeder.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SeederModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NDOE_ENV === 'test' ? '.env.test' : '.env',
+    }),
+    SeederModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
